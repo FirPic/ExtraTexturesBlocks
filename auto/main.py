bf172d6a85a4f9c5771bd066caebf5c030287c15
MathiasDev-sudo <tiago.792918@gmail.com>
2022-08-16 11:23:27 +0200

    upgrade auto script

--- auto/main.py
+++ auto/main.py
@@ -1,3 +1,4 @@
+import json
 
 
 def json_blockstates_model_loot_tables():
@@ -5,7 +6,7 @@ def json_blockstates_model_loot_tables():
     textures_folder = "arcane_stone"
     prefix_of_block_and_file = textures_folder+"_"
     list_of_blocks = ["array", "braid", "chaotic_bricks", "chaotic_medium", "chaotic_small", "circular", "cracked", "cracked_bricks", "cuts", "dent", "encased_bricks", "french_one", "french_two", "jellybean", "layers", "mosaic", "ornate", "panel", "pillar", "prism", "raw", "road", "slanted", "small_bricks", "soft_bricks", "solid_bricks", "tiles_large", "tiles_medium", "tiles_small", "triple_bricks", "twisted", "weaver", "zag"]
-
+    #list_of_blocks =["cc"]
 
     print('reading blocks list ')
 
@@ -15,28 +16,43 @@ def json_blockstates_model_loot_tables():
 
         print('create file of block : ' + file)
 
-        file_datastates = open("..\\src\\main\\resources\\assets\extratexturesblocks\\blockstates\\"+file ,"w+")
-        file_model_block = open("..\\src\\main\\resources\\assets\\extratexturesblocks\\models\\block\\"+file ,"w+")
-        file_model_item = open("..\\src\\main\\resources\\assets\\extratexturesblocks\\models\\item\\"+file ,"w+")
-        file_data_loot_tables = open("..\\src\\main\\resources\\data\\extratexturesblocks\\loot_tables\\blocks\\"+file ,"w+")
+        #file_datastates = open("..\\src\\main\\resources\\assets\extratexturesblocks\\blockstates\\"+file ,"w+")
+        #file_model_block = open("..\\src\\main\\resources\\assets\\extratexturesblocks\\models\\block\\"+file ,"w+")
+        #file_model_item = open("..\\src\\main\\resources\\assets\\extratexturesblocks\\models\\item\\"+file ,"w+")
+        #file_data_loot_tables = open("..\\src\\main\\resources\\data\\extratexturesblocks\\loot_tables\\blocks\\"+file ,"w+")
 
         print('write file of block : ' + file)
 
-        file_datastates.write('{"variants": {"": { "model":  "extratexturesblocks:block/'+block_name+'"}}}')
-        file_model_block.write('{"parent": "block/cube_all","textures": {"all": "extratexturesblocks:block/'+textures_folder+'/'+list_of_blocks[x]+'"}}')
-        file_model_item.write('{"parent": "extratexturesblocks:block/'+block_name+'"}')
-        file_data_loot_tables.write('{"type": "minecraft:block","pools": [{"rolls": 1,"entries":[{"type": "minecraft:item","name": "extratexturesblocks:'+block_name+'"}]}]}')
+        #file_datastates.write('{"variants": {"": { "model":  "extratexturesblocks:block/'+block_name+'"}}}')
+        #file_model_block.write('{"parent": "block/cube_all","textures": {"all": "extratexturesblocks:block/'+textures_folder+'/'+list_of_blocks[x]+'"}}')
+        #file_model_item.write('{"parent": "extratexturesblocks:block/'+block_name+'"}')
+        #file_data_loot_tables.write('{"type": "minecraft:block","pools": [{"rolls": 1,"entries":[{"type": "minecraft:item","name": "extratexturesblocks:'+block_name+'"}]}]}')
 
         print('close files of block : ' + file)
 
-        file_datastates.close()
-        file_model_block.close()
-        file_model_item.close()
-        file_data_loot_tables.close()
+        #file_datastates.close()
+        #file_model_block.close()
+        #file_model_item.close()
+        #file_data_loot_tables.close()
 
+        json_name(block_name)
+
+
+def json_name(blockName):
+
+    blockId = "block.extratexturesblocks."+blockName
+    for w in range(len("_")):
+        blockName = blockName.replace("_", " ")
+    blockName = blockName.title()
+
+    JSONdata = {blockId:blockName}
+
+    lang_en_us = open('..\\src\\main\\resources\\assets\\extratexturesblocks\\lang\\en_us.json', "w")
+    print(JSONdata)
 
 
 
 print('start etb generator json file to blockstates, model of block and item, loot tables')
 json_blockstates_model_loot_tables()
 print('finish etb generator json file to blockstates, model of block and item, loot tables')
+
--- src/main/java/fr/firpic/extratexturesblocks/EtbItemGroup.java
+++ src/main/java/fr/firpic/extratexturesblocks/EtbItemGroup.java
@@ -7,6 +7,14 @@ import net.minecraft.item.ItemStack;
 
 public class EtbItemGroup {
 
+    public static final ItemGroup EtbGroup = new ItemGroup("Extra Textures Blocks") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(EtbItemsRegistry.ARRAY.get());
+        }
+    };
+
+
     public static final ItemGroup AncientStoneGroup = new ItemGroup("Ancient Stone") {
         @Override
         public ItemStack createIcon() {
